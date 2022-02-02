0d6edd23ca8b222130855c799296e8bfd4e0c3c6
Vignesh Natarajan <vikiworks.io@gmail.com>
2022-02-02 10:18:52 +0100

    xlrd installer

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#-------------------------------------------------
+#
+#   Config file to install xlrd package.
+#
+#   Installer: install_py2pack_xlrd.sh
+#
+#
+#
+# --------------------------------------------------
+#               Author  : Natan @ Vignesh Natarajan
+#               Version : v1.0
+# --------------------------------------------------
+
+
+#Python2 Location
+PYTHON_DIR="$HOME/mypy2/bin/bin/python2"
+
+PACKAGE_NAME="xlrd"
---
+++ install_py2pack_xlrd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# -----------------------------------------------------
+#
+#   Python 2 Package Installer ( macOS 10.15.7 )
+#   
+#       Package Name: xlrd
+# -----------------------------------------------------
+#
+#               Author  : Natan @ Vignesh Natarajan
+#               Version : v1.0
+#
+# -----------------------------------------------------
+
+PYTHON_DIR="$HOME/mypy2/bin/bin/python2"
+
+source ./config.sh
+
+if [ $? -ne 0 ]; then
+    echo "error: importing config.sh"
+    exit 1
+fi
+
+cd $PACKAGE_NAME
+[ $? -ne 0 ] && { echo "error line ( ${LINENO} )"; exit 1; }
+
+if [ ! -f setup.py ]; then
+    echo "error: setup.py missing"
+    exit 1
+fi
+
+$PYTHON_DIR setup.py install
